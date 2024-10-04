c44a512d4eac2b1605cebfad3a73a9df6a9d3f6f
Melaty Az Zahrani <azzahranimelaty@gmail.com>
2024-10-04 10:36:22 +0700

    Add files via upload

---
+++ ContohVariabel18.java
@@ -0,0 +1,14 @@
+public class ContohVariabel18 {
+    public static void main (String[] args) {
+        String SalahSatuHobySayaAdalah = "Berenang";
+        boolean isPandai = true;
+        char jenisKelamin = 'P';
+        byte _umurSayaSekarang = 17;
+        double $ipk = 3.92, tinggi = 1.55;
+        System.out.println(SalahSatuHobySayaAdalah);
+        System.out.println("Apakah pandai? " + isPandai);
+        System.out.println("Jenis Kelamin: " + jenisKelamin);
+        System.out.println("Umurku saat ini: " + _umurSayaSekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk,tinggi));
+    }
+}
